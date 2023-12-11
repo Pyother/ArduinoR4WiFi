a296afcf82ccce73e869040ca4dc5b6f3a8022a3
pyother <piotr.dominik.sobol@gmail.com>
2023-12-11 22:01:58 +0100

    Simplify message handling

--- sketch_mqtt_client/sketch_mqtt_client.ino
+++ sketch_mqtt_client/sketch_mqtt_client.ino
@@ -1,9 +1,8 @@
 #include <WiFiS3.h>
 #include <ArduinoMqttClient.h>
-#include <Servo.h>
 
-#define SECRET_SSID ""
-#define SECRET_PASS ""
+#define SECRET_SSID "Tenda_3CA410"
+#define SECRET_PASS "waterduck934"
 #define MQTT_SERVER "test.mosquitto.org"
 #define MQTT_PORT 1833
 
@@ -24,8 +23,6 @@ MqttClient mqttClient(wifiClient);
 
 void setup() {
   Serial.begin(9600);
-  servo.attach(9);
-  servo.write(40);
   while (!Serial) {
     ; 
   }
@@ -46,7 +43,6 @@ void setup() {
     status = WiFi.begin(ssid, pass);
     delay(10000);
   }
-  Serial.println("Connected to WiFi");
   printWifiStatus();
 
   if(!mqttClient.connect(broker, port)) {
@@ -55,47 +51,31 @@ void setup() {
     while(1);
   }
   Serial.println("✓ Connected to MQTT broker.");
+  Serial.println();
   mqttClient.subscribe(topic);
 }
 
 void loop() {
   int messageSize = mqttClient.parseMessage();
   if (messageSize) {
-    // we received a message, print out the topic and contents
-    Serial.print("Received a message with topic '");
-    Serial.print(mqttClient.messageTopic());
-    Serial.print("', length ");
-    Serial.print(messageSize);
-    Serial.println(" bytes:");
+    Serial.print("Received a message: ");
 
-    // use the Stream interface to print the contents
     while (mqttClient.available()) {
       char value = mqttClient.read();
-      // 
+      Serial.println(value);
+      if(value == 's') {
+        function();
+      }
     }
-    Serial.println();
   }
+}
 
-  delay(3000);
-  mqttClient.beginMessage(topic);
-  mqttClient.print(random(1000));
-  mqttClient.endMessage();
+void function() {
+  Serial.println("S");
 }
 
 void printWifiStatus() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print your board's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
+  Serial.print("✓ Connected to network ");
+  Serial.println(ssid);
 }
 
